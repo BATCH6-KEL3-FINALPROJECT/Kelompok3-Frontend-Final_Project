50a44d0f26b5d2578ffa9babe0e194805829a240
Hafieddz <hasmy41@gmail.com>
2024-05-29 13:20:33 +0800

    Adding : Views in checkout page

---
+++ src/components/CheckoutForm.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import checklistIcon from "../../public/Checkout_Saved.svg";
+
+const CheckoutForm = ({ children, title, isSaved }) => {
+  return (
+    <div>
+      <div className="bg-[#3C3C3C] text-white py-2.5 px-4 rounded-tr-2xl rounded-tl-2xl flex justify-between items-center ">
+        {title}
+        {isSaved && <img src={checklistIcon} width={24} height={24} />}
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default CheckoutForm;
