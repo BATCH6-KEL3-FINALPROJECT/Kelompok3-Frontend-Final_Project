7a5b4349ecbb9f7ca9eb2343feda0ba5e1ec72bb
Hafieddz <hasmy41@gmail.com>
2024-06-05 22:58:30 +0800

    Adding : create promise in seatsDummy, and create function for checkout pages

--- src/utils/flightDummy.js
+++ src/utils/flightDummy.js
@@ -1,6 +1,6 @@
-const departure_time = new Date("2023-03-03T07:00:00");
+const departure_time = new Date("2023-03-03T07:00:00").toISOString();
 const departure_airport = "Soekarno Hatta - Terminal 1A Domestik";
-const arrive_time = new Date("2023-03-03T11:00:00");
+const arrive_time = new Date("2023-03-03T11:00:00").toISOString();
 const arrival_airport = "Melbourne International Airport";
 const flightDescription = [
   "Baggage 20kg",
