90ed0c11c571a8e266c0925fe3905e5df1889496
Hafieddz <hasmy41@gmail.com>
2024-06-14 19:58:05 +0800

    Fixing[style] : Fix responsive

--- src/components/CheckoutAlert.jsx
+++ src/components/CheckoutAlert.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const CheckoutAlert = ({ type, message }) => {
   return (
-    <div className="w-full flex justify-center px-3">
+    <div className="w-full flex justify-center px-5 md:px-32 lg:px-10">
       <div
         className={`w-[850px] max-w-[850px] ${
           type === "Danger" ? "bg-[#FF0000]" : "bg-[#73CA5C]"
-        } h-12 rounded-lg flex items-center justify-center text-white font-semibold`}
+        } h-12 rounded-lg flex items-center justify-center text-white font-semibold text-sm md:text-base`}
       >
         {message}
       </div>
