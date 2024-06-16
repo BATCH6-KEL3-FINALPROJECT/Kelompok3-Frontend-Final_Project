65b43eb063a3a6ac4f168beab41367d373b30272
Rendi Hardiartama <rendirenhard@gmail.com>
2024-06-16 20:31:34 +0700

    Fixing[style] : Order History

--- src/components/RiwayatCard.jsx
+++ src/components/RiwayatCard.jsx
@@ -1,61 +1,76 @@
 import React from "react";
 
-const RiwayatCard = ({ ticket }) => {
+const RiwayatCard = ({ ticket, selected }) => {
   const formatDate = (dateString) => {
     const options = { year: "numeric", month: "long", day: "numeric" };
-    return new Date(dateString).toLocaleDateString("en-US", options);
+    return new Date(dateString).toLocaleDateString("id-ID", options);
+  };
+
+  const getStatusStyle = (status) => {
+    switch (status.toLowerCase()) {
+      case "confirmed":
+        return "bg-[#73CA5C] text-white";
+      case "pending":
+        return "bg-[#FF0000] text-white";
+      case "cancelled":
+        return "bg-[#8A8A8A] text-white";
+      default:
+        return "bg-gray-200 text-gray-700";
+    }
   };
 
   return (
-    <div className="border-2 p-4 border-[#7126B5BF] rounded-xl shadow-md mb-4">
-      <p className="bg-[#73CA5C] text-white px-3 p-1 rounded-full">
+    <div
+      className={`p-4 rounded-xl shadow-md mb-4 ${
+        selected ? "border-2 border-[#7126B5BF]" : "border-2 border-gray-300"
+      }`}
+    >
+      <div
+        className={`${getStatusStyle(
+          ticket.ticket_status
+        )} px-3 py-1 rounded-full inline-block mb-1`}
+      >
         {ticket.ticket_status.charAt(0).toUpperCase() +
           ticket.ticket_status.slice(1)}
-      </p>
-      <div className="mt-4">
-        <div className="mb-4">
+      </div>
+      <div className="flex justify-between items-center mb-4">
+        <div className="text-center">
+          <h3 className="text-lg font-bold text-gray-900">
+            <span className="inline-block">
+              <img src="location.svg" alt="location" />
+            </span>{" "}
+            Jakarta
+          </h3>
+          <p className="text-gray-700">5 Maret 2023 19:10</p>
+        </div>
+        <div className="text-center">
+          <p className="text-gray-700">4h 0m</p>
+          <p>
+            <img src="arrow.svg" alt="" />
+          </p>
+        </div>
+        <div className="text-center">
+          <h3 className="text-lg font-bold text-gray-900">
+            <span className="inline-block">
+              <img src="location.svg" alt="location" />
+            </span>{" "}
+            Melbourne
+          </h3>
+          <p className="text-gray-700">5 Maret 2023 21:10</p>
+        </div>
+      </div>
+      <hr className="border-t-2 border-gray-200 mb-1" />
+      <div className="flex justify-between items-center mb-1">
+        <div>
           <h3 className="text-lg font-bold text-gray-900">Booking Code:</h3>
           <p className="text-gray-700">{ticket.booking_id}</p>
         </div>
-        <div className="flex justify-between items-center mb-4">
-          <div>
-            <h3 className="text-lg font-bold text-gray-900">Ticket Code:</h3>
-            <p className="text-gray-700">{ticket.ticket_code}</p>
-          </div>
-          <div>
-            <h3 className="text-lg font-bold text-gray-900">Passenger:</h3>
-            <p className="text-gray-700">{ticket.passenger_name}</p>
-          </div>
+        <div>
+          <h3 className="text-lg font-bold text-gray-900">Class:</h3>
+          <p className="text-gray-700">Economy</p>
         </div>
-        <div className="flex justify-between items-center mb-4">
-          <div>
-            <h3 className="text-lg font-bold text-gray-900">Flight:</h3>
-            <p className="text-gray-700">{ticket.flight_id}</p>
-          </div>
-          <div>
-            <h3 className="text-lg font-bold text-gray-900">Seat Number:</h3>
-            <p className="text-gray-700">{ticket.seat_number}</p>
-          </div>
-        </div>
-        <div className="flex justify-between items-center mb-4">
-          <div>
-            <h3 className="text-lg font-bold text-gray-900">Terminal:</h3>
-            <p className="text-gray-700">{ticket.TERMINAL}</p>
-          </div>
-          <div>
-            <h3 className="text-lg font-bold text-gray-900">Status:</h3>
-            <p className="text-gray-700">{ticket.ticket_status}</p>
-          </div>
-        </div>
-        <div className="flex justify-between items-center">
-          <div>
-            <h3 className="text-lg font-bold text-gray-900">Created At:</h3>
-            <p className="text-gray-700">{formatDate(ticket.createdAt)}</p>
-          </div>
-          <div>
-            <h3 className="text-lg font-bold text-gray-900">Updated At:</h3>
-            <p className="text-gray-700">{formatDate(ticket.updatedAt)}</p>
-          </div>
+        <div className="text-right">
+          <h3 className="text-lg font-bold text-gray-900">IDR 9.850.000</h3>
         </div>
       </div>
     </div>
--- src/pages/Riwayat.jsx
+++ src/pages/Riwayat.jsx
@@ -9,6 +9,10 @@ import Topnav from "../components/Topnav";
 import useSend from "../hooks/useSend";
 import { jwtDecode } from "jwt-decode";
 import RiwayatCard from "../components/RiwayatCard";
+import FlightDetails from "../components/FlightDetails";
+import CheckoutPricing from "../components/CheckoutPricing";
+import { flightDetails } from "../utils/flightDummy";
+import { passenger } from "../utils/generatePassanger";
 
 const Riwayat = () => {
   const { loading, sendData } = useSend();
@@ -84,6 +88,14 @@ const Riwayat = () => {
 
   const groupedTickets = groupByMonthYear(dataRiwayat);
 
+  const [flightDetail, setFlightDetail] = useState([]);
+  const [passengerInfo, setPassengerInfo] = useState([]);
+
+  useEffect(() => {
+    setFlightDetail(flightDetails());
+    setPassengerInfo(passenger.sort());
+  }, []);
+
   return (
     <>
       <Topnav isLogin={isLogin} isSearch={false} />
@@ -125,10 +137,10 @@ const Riwayat = () => {
             </button>
           </motion.div>
         </div>
-        <div className="flex gap-16">
+        <div className="flex gap-10">
           {Object.keys(groupedTickets).length > 0 ? (
             Object.entries(groupedTickets).map(([monthYear, tickets]) => (
-              <div key={monthYear} className="flex-grow mx-10">
+              <div key={monthYear} className="flex-grow mx-">
                 <h2 className="text-lg font-semibold mb-2">
                   {new Date(`${monthYear}-01`).toLocaleString("default", {
                     year: "numeric",
@@ -145,179 +157,9 @@ const Riwayat = () => {
               <p>No booking history found.</p>
             </div>
           )}
-          <div>
-            <div className="flex flex-col gap-1.5">
-              <div className="flex items-center justify-between gap-5">
-                <h3 className="text-gray-900 font-poppins text-lg font-bold">
-                  Detail Pesanan
-                </h3>
-                <p className="flex items-center justify-center rounded-full p-1 bg-[#73CA5C] px-4 text-center text-sm text-white">
-                  Issued
-                </p>
-              </div>
-              <div className="flex">
-                <h4 className="text-gray-900 font-poppins text-lg font-bold">
-                  <span className="font-normal text-gray-900">
-                    Booking Code:
-                  </span>
-                  <span className="text-[#7126B5]">6723y2GHK</span>
-                </h4>
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-15px">
-              <div className="flex items-start">
-                <div className="flex flex-1 flex-col items-start">
-                  <h5 className="text-gray-900 font-poppins w-33% text-sm font-bold leading-5 md:w-full">
-                    <span className="text-base text-gray-900">19:10</span>
-                    <br />
-                    <span className="font-normal text-gray-900">
-                      5 Maret 2023
-                    </span>
-                  </h5>
-                  <p className="text-gray-900 font-poppins text-sm font-medium">
-                    Soekarno Hatta - Terminal 1A Domestik
-                  </p>
-                </div>
-                <div className="relative ml-[-78px] flex">
-                  <h6 className="text-gray-900 font-poppins text-xs font-bold text-deep_purple-300">
-                    Keberangkatan
-                  </h6>
-                </div>
-              </div>
-              <img
-                className="h-px"
-                src="images/img_divider.svg"
-                alt="divider"
-                loading="lazy"
-              />
-            </div>
-
-            <div className="flex flex-col gap-2 pt-2">
-              <div className="flex">
-                <div className="flex flex-col justify-center py-16 md:py-5">
-                  <img
-                    className="h-24px object-cover"
-                    src="logoplane.svg"
-                    alt="Logo"
-                    loading="lazy"
-                  />
-                </div>
-                <p className="text-gray-900 font-poppins w-93% text-xs font-medium leading-18px">
-                  <span className="text-sm font-bold text-gray-900">
-                    Jet Air - Economy
-                    <br />
-                    JT - 203
-                    <br />
-                  </span>
-                  <span className="text-sm font-bold text-gray-900">
-                    Informasi:
-                  </span>
-                  <br />
-                  <span className="text-sm text-purple-900">
-                    Penumpang 1: Mr. Harry Potter
-                  </span>
-                  <br />
-                  <span className="text-sm font-normal text-gray-900">
-                    ID: 1234567
-                  </span>
-                  <br />
-                  <span className="text-sm text-purple-900">
-                    Penumpang 2: Miss Hermione
-                  </span>
-                  <br />
-                  <span className="text-sm font-normal text-gray-900">
-                    ID: 789658
-                  </span>
-                </p>
-              </div>
-              <img
-                className="h-px"
-                src="images/img_divider.svg"
-                alt="divider"
-                loading="lazy"
-              />
-            </div>
-
-            <div className="flex flex-col gap-3.5 pt-3">
-              <div>
-                <div className="flex items-start">
-                  <div className="flex flex-1 flex-col items-start gap-0.5">
-                    <p className="text-gray-900 font-poppins w-41% text-sm font-bold leading-5 md:w-full">
-                      <span className="text-gray-900">21:10</span>
-                      <br />
-                      <span className="font-normal text-gray-900">
-                        5 Maret 2023
-                      </span>
-                    </p>
-                    <p className="text-gray-900 font-poppins text-sm font-medium">
-                      Melbourne International Airport
-                    </p>
-                  </div>
-                  <div className="relative ml-[-24px] flex">
-                    <p className="text-gray-900 font-poppins text-xs font-bold text-deep_purple-300">
-                      Kedatangan
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <img
-                className="h-px"
-                src="images/img_divider.svg"
-                alt="divider"
-                loading="lazy"
-              />
-            </div>
-
-            <div className="flex flex-col gap-0.5 py-2">
-              <div className="flex">
-                <p className="text-gray-900 font-poppins text-sm font-bold">
-                  Rincian Harga
-                </p>
-              </div>
-              <div className="flex justify-between gap-5">
-                <div className="flex flex-1">
-                  <p className="text-gray-900 font-poppins text-sm font-normal">
-                    2 Adults
-                  </p>
-                </div>
-                <div className="flex">
-                  <p className="text-gray-900 font-poppins text-sm font-normal">
-                    IDR 9.550.000
-                  </p>
-                </div>
-              </div>
-              <div className="flex gap-2">
-                <div className="flex flex-1">
-                  <p className="text-gray-900 font-poppins text-sm font-normal">
-                    1 Baby
-                  </p>
-                </div>
-                <p className="text-gray-900 font-poppins text-sm font-normal">
-                  IDR 0
-                </p>
-              </div>
-              <div className="flex gap-2">
-                <div className="flex flex-1">
-                  <p className="text-gray-900 font-poppins text-sm font-normal">
-                    Tax
-                  </p>
-                </div>
-                <p className="text-gray-900 font-poppins text-sm font-normal">
-                  IDR 300.000
-                </p>
-              </div>
-              <div className="flex items-center gap-2 border-t border-solid border-blue-gray-100 py-2.5">
-                <div className="flex flex-1">
-                  <h6 className="text-gray-900 font-poppins text-base font-bold">
-                    Total
-                  </h6>
-                </div>
-                <h6 className="text-gray-900 font-poppins text-lg font-bold text-deep_purple-500">
-                  IDR 9.850.000
-                </h6>
-              </div>
-            </div>
+          <div className="px-5 lg:px-0">
+            <FlightDetails flightDetail={flightDetail} />
+            <CheckoutPricing passengerInfo={passengerInfo} />
           </div>
         </div>
       </div>
