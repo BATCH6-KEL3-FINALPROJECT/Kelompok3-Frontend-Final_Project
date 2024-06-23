d108be898e2418f6c950c1c18252538b708059a5
Rendi Hardiartama <rendirenhard@gmail.com>
2024-06-23 17:18:41 +0700

    Fixing[bug]: History Empty Component

--- src/components/HistoryEmpty.jsx
+++ src/components/HistoryEmpty.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const HistoryEmpty = () => {
+  const navigate = useNavigate();
+
+  const navigateToHome = () => {
+    navigate("/");
+  };
+
   return (
     <div className="flex justify-center items-center gap-2 flex-col min-h-[50vh] px-4 py-5 md:py-8">
       <div className="flex justify-center">
@@ -10,7 +17,10 @@ const HistoryEmpty = () => {
         <p className="text-[#7126B5]">Oops! Riwayat pesanan kosong!</p>
         <span>Anda belum melakukan pemesanan penerbangan</span>
       </h1>
-      <button className="btn btn-active bg-[#D0B7E6] w-full md:w-[347px] h-[48px] rounded-lg text-white hover:bg-[#7126B5]">
+      <button
+        onClick={navigateToHome}
+        className="btn btn-active bg-[#7126B5] w-full md:w-[347px] h-[48px] rounded-lg text-white hover:bg-[#8559ae]"
+      >
         Cari Penerbangan Lain
       </button>
     </div>
